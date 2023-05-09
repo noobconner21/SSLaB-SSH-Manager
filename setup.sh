591cd106c3f3e4a3e52c390d9e889302176198ac
Project SSLaB LK <94550161+noobconner21@users.noreply.github.com>
2023-05-09 06:28:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,4 +21,6 @@ sleep 5
 echo "Done .."
 sleep 2
 #https://github.com/noobconner21/Auto_Install_SSH_Manager/blob/main/etc.zip
-bash <(curl -Ls https://raw.githubusercontent.com/noobconner21/SSH-Panel/master/sshsetup.sh)
+#bash <(curl -Ls https://raw.githubusercontent.com/noobconner21/SSH-Panel/master/sshsetup.sh)
+
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/noobconner21/SSH-Panel/main/sshsetup.sh; chmod 777 sshsetup.sh;./sshsetup.sh
